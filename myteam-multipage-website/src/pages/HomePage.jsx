c34fc1e58a3c6c542703e92975f4f2dc3b9e3dac
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-02 18:37:26 +0100

    commit

--- myteam-multipage-website/src/pages/HomePage.jsx
+++ myteam-multipage-website/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Hero from '../components/homepage/Hero'
+import Services from '../components/homepage/Services'
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   )
 }
