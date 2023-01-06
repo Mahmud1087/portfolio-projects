1e1da5746b22cbed38827c39aa24405ad846ca7a
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-06 07:22:59 +0100

    feat: implemented the testimonial section

---
+++ myteam-multipage-website/src/components/styles/homepage/Testimonial.Styled..jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+import homeBG3 from '../../../assets/homeBG3.png'
+import testimonialBG from '../../../assets/“.png'
+
+export const TestimonialStyled = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: url(${homeBG3});
+  background-position: center;
+  background-size: cover;
+  padding: 0 10rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.8rem;
+  max-width: 85%;
+  margin: 0 auto;
+  color: #ffffff;
+  line-height: 100%;
+  text-align: center;
+
+  span {
+    color: #79c8c7;
+  }
+`
+
+export const TestimonialsContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  margin-top: 5rem;
+
+  .testimonials {
+    background: url(${testimonialBG});
+    background-repeat: no-repeat;
+    background-position: top;
+    display: grid;
+    place-items: center;
+    row-gap: 2rem;
+    text-align: center;
+
+    .texts {
+      margin-top: 2.5rem;
+      font-size: 14px;
+      color: #ffffff;
+    }
+
+    .author-details {
+      .author {
+        color: #79c8c7;
+        font-size: 14px;
+        margin-bottom: 0.3rem;
+      }
+
+      .author-title {
+        font-size: 12px;
+        color: #fff;
+      }
+    }
+
+    img {
+      width: 3rem;
+    }
+  }
+`
--- myteam-multipage-website/src/pages/HomePage.jsx
+++ myteam-multipage-website/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/homepage/Hero'
 import Services from '../components/homepage/Services'
+import Testimonial from '../components/homepage/Testimonial'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <Testimonial />
     </>
   )
 }
