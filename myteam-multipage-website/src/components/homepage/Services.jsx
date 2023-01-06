27ad806f491f47d92a08df5be58509ceeaca5111
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-06 06:09:07 +0100

    feat: implemented the services section

--- myteam-multipage-website/src/components/homepage/Services.jsx
+++ myteam-multipage-website/src/components/homepage/Services.jsx
@@ -1,3 +1,60 @@
+import {
+  Container,
+  LeftContent,
+  RightContent,
+  ServiceStyled,
+} from '../styles/homepage/Services.Styled'
+
+import image1 from '../../assets/Group 11.svg'
+import image2 from '../../assets/Group 12.svg'
+import image3 from '../../assets/Group 13.svg'
+
 export default function Services() {
-  return <div>Services</div>
+  const data = [
+    {
+      id: 1,
+      icon: image1,
+      title: 'Experienced Individuals',
+      text: 'Our network is made up of highly experienced professionals who are passionate about what they do.',
+    },
+    {
+      id: 2,
+      icon: image2,
+      title: 'Easy to Implement',
+      text: 'Our processes have been refined over years of implementation meaning our teams always deliver.',
+    },
+    {
+      id: 3,
+      icon: image3,
+      title: 'Enhanced Productivity',
+      text: 'Our customized platform with in-built analytics helps you manage your distributed teams.',
+    },
+  ]
+
+  return (
+    <ServiceStyled>
+      <Container>
+        <LeftContent>
+          <hr />
+          <h1>Build & manage distributed teams like no one else.</h1>
+        </LeftContent>
+        <RightContent>
+          {data.map((item) => {
+            const { id, title, text, icon } = item
+            return (
+              <div className='container' key={id}>
+                <div>
+                  <img src={icon} alt={`icon ${id}`} />
+                </div>
+                <div className='textContent'>
+                  <h4>{title}</h4>
+                  <p>{text}</p>
+                </div>
+              </div>
+            )
+          })}
+        </RightContent>
+      </Container>
+    </ServiceStyled>
+  )
 }
--- myteam-multipage-website/src/components/styles/homepage/Services.Styled.jsx
+++ myteam-multipage-website/src/components/styles/homepage/Services.Styled.jsx
@@ -1,3 +1,72 @@
 import styled from 'styled-components'
+import homeBG2 from '../../../assets/homeBG2.png'
 
-export const ServicesStyled = styled.section``
+export const ServiceStyled = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+  background: url(${homeBG2});
+  background-position: center;
+  background-size: cover;
+  padding: 0 10rem;
+`
+
+export const Container = styled.div`
+  display: flex;
+  gap: 8rem;
+`
+
+export const LeftContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+
+  hr {
+    width: 3rem;
+    background-color: #f67e7e;
+    border: none;
+    height: 0.2rem;
+  }
+
+  h1 {
+    margin-top: 1.5rem;
+    font-size: 3rem;
+    font-weight: 700;
+    color: white;
+    line-height: 100%;
+  }
+`
+
+export const RightContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.8rem;
+  margin-top: 3.7rem;
+  .container {
+    display: flex;
+    gap: 1.5rem;
+
+    img {
+      width: 3.5rem;
+    }
+
+    .textContent {
+      display: flex;
+      flex-direction: column;
+      row-gap: 0.6rem;
+
+      h4 {
+        color: #f67e7e;
+        font-size: 14px;
+      }
+
+      p {
+        color: white;
+        font-size: 13px;
+      }
+    }
+  }
+`
