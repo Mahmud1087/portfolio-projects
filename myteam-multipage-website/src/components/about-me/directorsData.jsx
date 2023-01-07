749ef2d8b426c114510abb4feb504c0d479d2f9b
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-07 14:39:55 +0100

    added the file for the data file for the director details

---
+++ myteam-multipage-website/src/components/about-me/directorsData.jsx
@@ -0,0 +1,27 @@
+import aboutImg1 from '../../../assets/aboutImg1.png'
+import aboutImg2 from '../../../assets/aboutImg2.png'
+import aboutImg3 from '../../../assets/aboutImg3.png'
+import aboutImg4 from '../../../assets/aboutImg4.png'
+import aboutImg5 from '../../../assets/aboutImg5.png'
+import aboutImg6 from '../../../assets/aboutImg6.png'
+
+const details = [
+  { id: 1, image: aboutImg1, name: 'Nikita Marks', title: 'Founder and CEO' },
+  {
+    id: 2,
+    image: aboutImg2,
+    name: 'Christian Duncan',
+    title: 'Co-founder and COO',
+  },
+  { id: 3, image: aboutImg3, name: 'Cruz Hamer', title: 'Co-founder and CTO' },
+  {
+    id: 4,
+    image: aboutImg4,
+    name: 'Drake Heaton',
+    title: 'Business Development Lead',
+  },
+  { id: 5, image: aboutImg5, name: 'Griffin Wise', title: 'Marketing Lead' },
+  { id: 6, image: aboutImg6, name: 'Aden Allen', title: 'Head of talent' },
+]
+
+export { details }
