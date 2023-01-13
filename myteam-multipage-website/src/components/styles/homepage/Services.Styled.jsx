36795295983fc2a1dd286c094f8c506e7bbc19c5
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-13 17:51:20 +0100

    added the tablet styling to the homepage services section

--- myteam-multipage-website/src/components/styles/homepage/Services.Styled.jsx
+++ myteam-multipage-website/src/components/styles/homepage/Services.Styled.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import homeBG2 from '../../../assets/homeBG2.png'
+import tabHomeBG2 from '../../../assets/tabHomeBG2.png'
 
 export const ServiceStyled = styled.section`
   width: 100%;
@@ -10,11 +11,23 @@ export const ServiceStyled = styled.section`
   background-position: center;
   background-size: cover;
   padding: 0 10rem;
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    height: 68vh;
+    padding: 0 5rem;
+    background: url(${tabHomeBG2});
+    background-repeat: no-repeat;
+  }
 `
 
 export const Container = styled.div`
   display: flex;
   gap: 8rem;
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    flex-direction: column;
+    row-gap: 1rem;
+  }
 `
 
 export const LeftContent = styled.div`
@@ -36,6 +49,18 @@ export const LeftContent = styled.div`
     font-weight: 700;
     color: white;
     line-height: 100%;
+  }
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    hr {
+      height: 0.3rem;
+    }
+
+    h1 {
+      font-size: 2rem;
+      margin-top: 0.5rem;
+      max-width: 30rem;
+    }
   }
 `
 
@@ -60,13 +85,31 @@ export const RightContent = styled.div`
 
       h4 {
         color: #f67e7e;
-        font-size: 14px;
+        font-size: 0.87rem;
       }
 
       p {
         color: white;
         opacity: 0.8;
-        font-size: 13px;
+        font-size: 0.8rem;
+      }
+    }
+  }
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    .container {
+      img {
+        width: 5rem;
+      }
+
+      .textContent {
+        row-gap: 0.8rem;
+
+        h4,
+        p {
+          font-size: 1.05rem;
+          line-height: 1.5;
+        }
       }
     }
   }
