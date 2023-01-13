b98327a17be9965d986e07dfb49efb57cb12286c
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-13 20:42:17 +0100

    added footer to the contact page

--- myteam-multipage-website/src/components/contact-me/ContactHero.jsx
+++ myteam-multipage-website/src/components/contact-me/ContactHero.jsx
@@ -7,31 +7,36 @@ import { Button, Logo, Navbar, NavLinks } from '../styles/homepage/Hero.Styled'
 import logo from '../../assets/myteam 2.svg'
 import ContactText from './ContactText'
 import ContactForm from './ContactForm'
+import Footer from '../homepage/Footer'
 export default function ContactHero() {
   return (
-    <ContactHeroSection>
-      <Navbar>
-        <div style={{ display: 'flex', gap: '3.5rem' }}>
-          <Link to='/about'>
-            <Logo src={logo} />
+    <>
+      {' '}
+      <ContactHeroSection>
+        <Navbar>
+          <div style={{ display: 'flex', gap: '3.5rem' }}>
+            <Link to='/about'>
+              <Logo src={logo} />
+            </Link>
+            <NavLinks>
+              <li>
+                <Link to='/'>home</Link>
+              </li>
+              <li>
+                <Link to='../about'>about</Link>
+              </li>
+            </NavLinks>
+          </div>
+          <Link to='contact'>
+            <Button>contact us</Button>
           </Link>
-          <NavLinks>
-            <li>
-              <Link to='/'>home</Link>
-            </li>
-            <li>
-              <Link to='../about'>about</Link>
-            </li>
-          </NavLinks>
-        </div>
-        <Link to='contact'>
-          <Button>contact us</Button>
-        </Link>
-      </Navbar>
-      <Container>
-        <ContactText />
-        <ContactForm />
-      </Container>
-    </ContactHeroSection>
+        </Navbar>
+        <Container>
+          <ContactText />
+          <ContactForm />
+        </Container>
+      </ContactHeroSection>
+      <Footer />
+    </>
   )
 }
