ff72b132ee2d4ef7ddbdc23fcefc851f88a88ccf
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-13 20:29:18 +0100

    added the tablet styling to the contactpage hero section

--- myteam-multipage-website/src/components/styles/contact-me/ContactHero.styled.jsx
+++ myteam-multipage-website/src/components/styles/contact-me/ContactHero.styled.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import contactBG from '../../../assets/contactBG.png'
+import tabContactBG from '../../../assets/tabContactBG.png'
 
 export const ContactHeroSection = styled.section`
   width: 100%;
@@ -8,6 +9,12 @@ export const ContactHeroSection = styled.section`
   background-position: center;
   background-size: cover;
   padding: 0 10rem;
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    height: 118vh;
+    padding: 0 3rem;
+    background: url(${tabContactBG});
+  }
 `
 
 export const Container = styled.div`
@@ -17,4 +24,10 @@ export const Container = styled.div`
   align-items: center;
   gap: 2rem;
   margin-top: 2rem;
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    flex-direction: column;
+    row-gap: 2rem;
+    text-align: center;
+  }
 `
