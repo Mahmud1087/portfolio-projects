464072f9a557f1d1d5220e02a957bd0cf4396607
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-13 20:01:52 +0100

    added the tablet styling to the aboutpage clients section

--- myteam-multipage-website/src/components/styles/about-me/Clients.styled.jsx
+++ myteam-multipage-website/src/components/styles/about-me/Clients.styled.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import aboutBG3 from '../../../assets/aboutBG3.png'
+import tabAboutBG3 from '../../../assets/tabAboutBG3.png'
 
 export const ClientsSection = styled.section`
   width: 100%;
@@ -12,6 +13,13 @@ export const ClientsSection = styled.section`
   background-size: cover;
   background-repeat: no-repeat;
   padding: 0 10rem;
+
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    height: 30vh;
+    background: url(${tabAboutBG3});
+    background-repeat: no-repeat;
+    padding: 0 3rem;
+  }
 `
 
 export const ClientsContainer = styled.div`
