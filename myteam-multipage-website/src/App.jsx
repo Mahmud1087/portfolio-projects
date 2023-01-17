5098c39289c74427d169850f90c27f4cacfc1092
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-17 17:29:51 +0100

    added styled component for the sidebar and set a state for the sidebar in the app component

--- myteam-multipage-website/src/App.jsx
+++ myteam-multipage-website/src/App.jsx
@@ -3,15 +3,27 @@ import AboutPage from './pages/AboutPage'
 import ContactMe from './pages/ContactMe'
 import { GlobalStyles } from './components/styles/GlobalStyles.styled'
 import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function App() {
+  const [isSideBarOPen, seIsSideBarOpen] = useState(false)
+
   return (
     <>
       <GlobalStyles />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='about' element={<AboutPage />} />
-        <Route path='contact' element={<ContactMe />} />
+        <Route
+          path='/'
+          element={<HomePage open={isSideBarOPen} setOpen={seIsSideBarOpen} />}
+        />
+        <Route
+          path='about'
+          element={<AboutPage open={isSideBarOPen} setOpen={seIsSideBarOpen} />}
+        />
+        <Route
+          path='contact'
+          element={<ContactMe open={isSideBarOPen} setOpen={seIsSideBarOpen} />}
+        />
       </Routes>
     </>
   )
