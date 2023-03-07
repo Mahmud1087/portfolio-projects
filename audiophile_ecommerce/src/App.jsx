e528e9aeb3a7f4df1214c712aba284fed905d10e
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-03-07 15:58:10 +0100

    added customization to the tailwind config file

--- audiophile_ecommerce/src/App.jsx
+++ audiophile_ecommerce/src/App.jsx
@@ -1,3 +1,11 @@
+import { Navbar } from './components/main'
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className='bg-dark-200'>
+      <div className='container w-[1109.83px]'>
+        <Navbar />
+      </div>
+    </div>
+  )
 }
--- audiophile_ecommerce/tailwind.config.cjs
+++ audiophile_ecommerce/tailwind.config.cjs
@@ -2,6 +2,29 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px',
+    },
+    colors: {
+      light: {
+        100: '#FFFFFF',
+        200: '#FAFAFA',
+        300: '#F1F1F1',
+      },
+      dark: {
+        100: '#101010',
+        200: '#000000',
+      },
+      c_orange: {
+        100: '#FBAF85',
+        200: '#D87D4A',
+      },
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
