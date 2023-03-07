0eb0c305bd0be3941a14f313ecf6b37bf7c76fda
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-03-07 12:36:57 +0100

    new project

--- myteam-multipage-website/src/pages/ContactMe.jsx
+++
@@ -1,9 +0,0 @@
-import ContactHero from '../components/contact-me/ContactHero'
-
-export default function ContactMe({ open, setOpen }) {
-  return (
-    <>
-      <ContactHero open={open} setOpen={setOpen} />
-    </>
-  )
-}
