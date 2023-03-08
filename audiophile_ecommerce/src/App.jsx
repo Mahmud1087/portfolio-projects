fc452150099794271e02e3df9a7e94db006ee13b
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-03-08 11:31:13 +0100

    feat: navbar completed

--- audiophile_ecommerce/src/App.jsx
+++ audiophile_ecommerce/src/App.jsx
@@ -1,11 +1,29 @@
-import { Navbar } from './components/main'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Error, Navbar } from './components/main'
+import {
+  EarphonesPage,
+  HeadphonesPage,
+  Homepage,
+  ProductsPage,
+  SingleproductPage,
+  SpeakersPage,
+} from './pages/main'
 
 export default function App() {
   return (
-    <div className='bg-dark-200'>
-      <div className='container w-[1109.83px]'>
-        <Navbar />
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+      {/* <Sidebar /> */}
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='headphones' element={<HeadphonesPage />} />
+        <Route path='speakers' element={<SpeakersPage />} />
+        <Route path='earphones' element={<EarphonesPage />} />
+        <Route path='products' element={<ProductsPage />} />
+        <Route path='products/:id' element={<SingleproductPage />} />
+        <Route path='error' element={<Error />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
   )
 }
--- audiophile_ecommerce/src/components/Navbar.jsx
+++ audiophile_ecommerce/src/components/Navbar.jsx
@@ -1,5 +1,47 @@
-import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { links } from '../data'
+import logo from '../assets/logo.svg'
+import cartImage from '../assets/cart.png'
 
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <div className='bg-dark-100'>
+      <section className='container text-light-100 lg:w-[1109.83px] md:w-[689px] sm:w-[327px]'>
+        <nav
+          className='border-b-light-200 flex
+       justify-between items-center md:py-6 sm:py-5 lg:py-8'>
+          <div className='flex items-center'>
+            <FaBars className='text-lg sm:text-[1rem] sm:mr-24 md:text-sm md:mr-8 lg:hidden' />
+            <img
+              src={logo}
+              alt='audiophile logo'
+              className='sm:w-[6.5rem] lg:w-[9.5rem] cursor-pointer'
+            />
+          </div>
+
+          <ul className='hidden lg:flex lg:gap-12 lg:items-center'>
+            {links.map((link) => {
+              const { id, text, url } = link
+              return (
+                <li key={id}>
+                  <Link
+                    to={url}
+                    className='tracking-[2px] uppercase text-[13px] hover:text-c_orange-100 transition-all'>
+                    {text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+
+          <img
+            src={cartImage}
+            alt='cart image'
+            className='sm:w-[1.1rem] lg:w-[1.8rem] cursor-pointer'
+          />
+        </nav>
+      </section>
+    </div>
+  )
 }
