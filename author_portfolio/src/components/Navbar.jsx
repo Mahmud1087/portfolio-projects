5afc7c5d578659faf2b11c7988c8c979d8be9b02
Mahmud1087 <abdulazeezm578@gmail.com>
2024-02-06 13:25:01 +0100

    started the navbar

--- author_portfolio/src/components/Navbar.jsx
+++ author_portfolio/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import logo from '../assets/logoo.png';
 function Navbar() {
   return (
     <nav>
-      <img src={logo} alt='Logo' className='w-10' />
+      <div className='w-[70vw] m-auto py-6 flex justify-end'>
+        <img src={logo} alt='Logo' className='w-12' />
+      </div>
     </nav>
   );
 }
