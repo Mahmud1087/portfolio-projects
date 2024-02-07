3f59cdb4762836f2742da7139e218365887d0b4d
Mahmud1087 <abdulazeezm578@gmail.com>
2024-02-07 22:13:40 +0100

    edited the BuyMyBook component

--- author_portfolio/src/components/BuyMyBook.jsx
+++ author_portfolio/src/components/BuyMyBook.jsx
@@ -1,12 +1,23 @@
+import amazonLogo from '../assets/amazon.png';
+import selarLogo from '../assets/selar.png';
+
 const BuyMyBook = () => {
   return (
     <div className='bg-[#f3f3f3]'>
-      <section className='container grid place-items-center'>
+      <section className='w-[30vw] m-auto py-[5rem] grid place-items-center'>
         <h1 className='heading'>Buy My Book</h1>
         <p className=' text-slate-500 text-sm mt-3 font-poppins'>
           Get the book from any of these great retailers
         </p>
-        <aside className='flex justify-between items-center'></aside>
+        <aside className='flex items-center justify-evenly w-full mt-10'>
+          <a href='amazon_link'>
+            <img src={amazonLogo} alt='Amazon Logo' className='w-[5rem]' />
+          </a>
+          <a href='https://www.amab.com.ng/products/dare-to-thrive'>amab</a>
+          <a href='selar_link'>
+            <img src={selarLogo} alt='Selar Logo' className='w-16' />
+          </a>
+        </aside>
       </section>
     </div>
   );
