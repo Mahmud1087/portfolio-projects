8820d70eee81dae53bc0137b1de6be1ac5e6d42a
Mahmud1087 <abdulazeezm578@gmail.com>
2024-02-07 21:53:07 +0100

    edited the quote component

--- author_portfolio/src/components/Quote.jsx
+++ author_portfolio/src/components/Quote.jsx
@@ -2,18 +2,21 @@ import { FaQuoteRight } from 'react-icons/fa';
 
 const Quote = () => {
   return (
-    <article className=' bg-[#f3f3f3] py-16 flex items-center justify-center '>
-      <aside className='flex flex-col gap-6 w-86 text-slate-700'>
+    <article className=' bg-[#f3f3f3] flex items-center justify-center '>
+      <aside className='container flex flex-col gap-6 w-86 text-slate-700'>
         <div className='w-full p-[1px] bg-gray-400 rounded-full'></div>
         <blockquote className='px-10 text-center font-chelsea italic'>
-          Living a life with passion and pursuing dreams is a life well lived
+          Iron rusts. Seasons shift. Situations change. And people grow. Some,
+          consciously evolving. Other drifting through life. We're Muslims.
+          We're here for a purpose. Choose conscious growth. Embrace intentional
+          living
         </blockquote>
         <section className='flex gap-6 h-[1px] items-center'>
           <div className='w-full p-[1px] bg-gray-400 rounded-full'></div>
-          <FaQuoteRight className='text-4xl text-slate-700' />
+          <FaQuoteRight className='text-5xl text-slate-700' />
           <div className='w-full p-[1px] bg-gray-400 rounded-full'></div>
         </section>
-        <p className='text-center text-sm font-lilita'>Umm Noorayn</p>
+        <p className='text-center text-lg font-lilita'>Umm Noorayn</p>
       </aside>
     </article>
   );
