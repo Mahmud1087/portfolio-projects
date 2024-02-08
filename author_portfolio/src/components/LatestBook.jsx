93eedae7567372cce898f7f89f4a3f91a3efb351
Mahmud1087 <abdulazeezm578@gmail.com>
2024-02-08 08:23:39 +0100

    made the navbar, hero, quotes and latest book sections mobile responsive

--- author_portfolio/src/components/LatestBook.jsx
+++ author_portfolio/src/components/LatestBook.jsx
@@ -4,8 +4,8 @@ import latest_book from '../assets/frontCover.jpg';
 const LatestBook = () => {
   return (
     <div id='latestbook' className='bg-[#f3f3f3] pb-[4rem]'>
-      <section className='container flex gap-24 '>
-        <div className='w-2/3 flex flex-col justify-center gap-5 font-poppins text-sm text-slate-500 leading-6'>
+      <section className='container-md-others flex-col lg:flex-row lg:container flex gap-24 '>
+        <div className='w-full lg:w-2/3 flex flex-col justify-center gap-5 font-poppins text-sm text-slate-500 leading-6'>
           <aside className='w-full leading-10'>
             <h1 className='heading'>My Latest Book — "Dare To Thrive"</h1>
             <p>On Sale since Jan 28, 2024</p>
@@ -28,7 +28,7 @@ const LatestBook = () => {
           </p>
         </div>
 
-        <article className='w-1/3 shadow-2xl shadow-gray-500'>
+        <article className='w-full lg:w-1/3 shadow-2xl shadow-gray-500'>
           <img src={latest_book} alt='my latest book cover' />
         </article>
       </section>
