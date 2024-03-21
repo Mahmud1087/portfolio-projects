ba0a5b5a5cf0bb445134bea9ac50c1ee1b54e83f
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-21 15:34:58 +0100

    changed the navbar border bottom color and also the border to 2px

--- audiophile_ecommerce/src/components/Navbar.jsx
+++ audiophile_ecommerce/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <div className='bg-[#141414] h-20 flex items-center justify-center'>
-      <section className='flex items-center justify-between h-[inherit] container border-b border-b-[#979797]'>
+      <section className='flex items-center justify-between h-[inherit] container border-b-2 border-b-[#ffffff2a]'>
         <div className='flex items-center gap-x-44 text-light-100'>
           <Logo />
           <ul className='flex items-center gap-10'>
