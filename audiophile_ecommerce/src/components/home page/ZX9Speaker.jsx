3a41fb27fd91728cb7c744c26ee7899a3c056f36
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-22 22:32:28 +0100

    added background color to the ZX9Speaker component in the home page

--- audiophile_ecommerce/src/components/home page/ZX9Speaker.jsx
+++ audiophile_ecommerce/src/components/home page/ZX9Speaker.jsx
@@ -1,4 +1,8 @@
 const ZX9Speaker = () => {
-  return <div>ZX9Speaker</div>;
+  return (
+    <div className='container'>
+      <section className='w-full h-[30rem] rounded-md bg-orange-200'></section>
+    </div>
+  );
 };
 export default ZX9Speaker;
