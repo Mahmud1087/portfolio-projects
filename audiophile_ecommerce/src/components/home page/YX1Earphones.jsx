2843a2af044663241d848a8e0e8a530ed7e6ef5e
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-22 22:53:19 +0100

    added the YX1Earphones component in the home page

--- audiophile_ecommerce/src/components/home page/YX1Earphones.jsx
+++ audiophile_ecommerce/src/components/home page/YX1Earphones.jsx
@@ -1,4 +1,17 @@
+import airpodImg from '../../assets/ear-pod.png';
+
 const YX1Earphones = () => {
-  return <div>YX1Earphones</div>;
+  return (
+    <div className='flex container gap-x-6 pt-12'>
+      <section className='w-1/2 rounded-md h-[18rem]'>
+        <img
+          src={airpodImg}
+          alt='Earbud image'
+          className='w-full h-full object-contain rounded-[inherit]'
+        />
+      </section>
+      <section className='w-1/2 rounded-md h-[18rem] bg-light-300'></section>
+    </div>
+  );
 };
 export default YX1Earphones;
