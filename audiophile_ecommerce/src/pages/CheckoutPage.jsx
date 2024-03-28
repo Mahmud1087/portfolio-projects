27e15f67f00b98dd5f167a6f43dc7cfc5706cfdc
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-28 15:21:02 +0100

    edited checkout page

--- audiophile_ecommerce/src/pages/CheckoutPage.jsx
+++ audiophile_ecommerce/src/pages/CheckoutPage.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function CheckoutPage() {
-  return <div>CheckoutPage</div>;
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
+  return (
+    <div className='container pt-[6rem]'>
+      <button
+        onClick={goBack}
+        className='text-[15px] text-[#00000062] mb-16 font-bold tracking-[1px]'
+      >
+        Go Back
+      </button>
+
+      <div className='mb-[11rem]'></div>
+    </div>
+  );
 }
