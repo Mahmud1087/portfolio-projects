318e7d64da5f20a1ff5d6d1300af89e2877b8667
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-01 18:15:55 +0100

    tablet responsive for YX1Earphones component

--- audiophile_ecommerce/src/components/home page/YX1Earphones.jsx
+++ audiophile_ecommerce/src/components/home page/YX1Earphones.jsx
@@ -1,19 +1,27 @@
-import airpodImg from '../../assets/ear-pod.png';
+import airpodImg1 from '../../assets/ear-pod.png';
+import airpodImg2 from '../../assets/ear-pod2.jpg';
 import { SeeProductButton } from '../../components/main';
 
 const YX1Earphones = () => {
   return (
-    <div className='flex container gap-x-6 pt-12'>
+    <div className='flex container lg:gap-x-6 pt-12'>
       <section className='w-1/2 rounded-lg h-[18rem]'>
         <img
-          src={airpodImg}
+          src={airpodImg1}
           alt='Earbud image'
-          className='w-full h-full object-contain rounded-[inherit]'
+          className='hidden w-full h-full object-contain rounded-[inherit] lg:block'
+        />
+        <img
+          src={airpodImg2}
+          alt='Earbud image'
+          className='w-full h-full object-contain rounded-[inherit] lg:hidden'
         />
       </section>
-      <section className='w-1/2 rounded-lg h-[18rem] bg-light-300 pl-24 flex items-center'>
-        <div className='flex flex-col gap-6 justify-center'>
-          <h1 className='text-2xl font-bold tracking-[1px]'>YX1 EARPHONES</h1>
+      <section className='w-1/2 rounded-lg h-[18rem] bg-light-300 flex items-center pl-8 lg:pl-24'>
+        <div className='flex flex-col justify-center gap-12 lg:gap-6'>
+          <h1 className='text-[2rem] font-bold tracking-[1px] text-center lg:text-start lg:text-2xl'>
+            YX1 EARPHONES
+          </h1>
           <SeeProductButton
             to='YX1'
             classes='border border-dark-300 bg-transparent hover:bg-dark-300 hover:text-light-100 text-dark-300 font-bold'
