517fb9d10d2ac41a8a45d78df0941105b256186f
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-02 16:17:31 +0100

    completed the tablet responsive for the single product page

--- audiophile_ecommerce/src/pages/SingleproductPage.jsx
+++ audiophile_ecommerce/src/pages/SingleproductPage.jsx
@@ -28,20 +28,25 @@ export default function SingleproductPage() {
         <ProductDetails data={data} />
       </div>
 
-      <section className='flex justify-between'>
-        <div className='flex flex-col gap-10 w-[55%]'>
-          <h1 className='text-3xl font-bold'>FEATURES</h1>
-          <article className='flex flex-col gap-8 text-[14.5px] text-[#00000077] font-semibold'>
+      <section className='flex flex-col justify-between lg:flex-row'>
+        <div className='flex flex-col gap-10 w-full lg:w-[55%]'>
+          <h1 className='text-[2.5rem] font-bold lg:text-3xl'>FEATURES</h1>
+          <article className='flex flex-col gap-8 text-[1.35rem] lg:text-[14.5px] text-[#00000077] font-semibold'>
             <p>{data.features.point1}</p>
             <p>{data.features.point2}</p>
           </article>
         </div>
-        <div className='flex flex-col gap-10 w-[45%] pl-[8rem]'>
-          <h1 className='text-3xl font-bold text-start'>IN THE BOX</h1>
+        <div className='flex gap-10 mt-28 lg:flex-col lg:mt-0 lg:w-[45%] lg:pl-[8rem]'>
+          <h1 className='text-[2.5rem] font-bold text-start lg:text-3xl'>
+            IN THE BOX
+          </h1>
           <aside className='flex flex-col gap-2'>
             {data.box.map((prod, i) => {
               return (
-                <div key={i} className='flex gap-6 text-[14.5px]'>
+                <div
+                  key={i}
+                  className='flex gap-6 text-[1.35rem] ml-24 mt-4 lg:mt-0 lg:text-[14.5px] lg:ml-0'
+                >
                   <h1 className='text-orange-200 font-bold'>{prod.size}</h1>
                   <p className='text-[#00000077] font-semibold'>{prod.item}</p>
                 </div>
@@ -51,12 +56,12 @@ export default function SingleproductPage() {
         </div>
       </section>
 
-      <article className='w-full h-[37rem] flex gap-8 mt-[8.5rem]'>
-        <div className='w-[45%] h-full flex flex-col justify-between'>
-          <img src={data.images.image1} alt='images' className='' />
-          <img src={data.images.image2} alt='images' className='' />
+      <article className='w-full h-[30rem] flex gap-4 mt-[8.5rem] lg:gap-8 lg:h-[37rem]'>
+        <div className='w-[40%] h-full flex flex-col justify-between gap-4 lg:gap-0 lg:w-[45%]'>
+          <img src={data.images.image1} alt='images' className='h-full' />
+          <img src={data.images.image2} alt='images' className='h-full' />
         </div>
-        <div className='w-[55%] h-full rounded-md'>
+        <div className='w-[60%] h-full rounded-md lg:w-[55%]'>
           <img
             src={data.images.image3}
             alt='images'
@@ -66,20 +71,20 @@ export default function SingleproductPage() {
       </article>
 
       <section className='mt-[11rem] mb-[4.5rem]'>
-        <h1 className='mb-12 text-center font-bold text-3xl tracking-[1px]'>
+        <h1 className='mb-12 text-center font-bold text-[2.5rem] tracking-[1px] lg:text-3xl'>
           YOU MAY ALSO LIKE
         </h1>
-        <div className='grid grid-cols-3 gap-9'>
+        <div className='grid grid-cols-3 gap-4 lg:gap-9'>
           {data.mayAlsoLike.map((prod, i) => {
             return (
               <div
                 key={i}
                 className='flex flex-col justify-center items-center gap-8'
               >
-                <aside className='flex justify-center items-center h-[18rem] rounded-md bg-light-300 w-full'>
-                  <img src={prod.img} alt='' className='w-[8rem]' />
+                <aside className='flex justify-center items-center h-[22rem] rounded-md bg-light-300 w-full lg:h-[18rem]'>
+                  <img src={prod.img} alt='' className='w-[9rem] lg:w-[8rem]' />
                 </aside>
-                <h1 className='font-bold text-xl mt-3 tracking-[1px]'>
+                <h1 className='font-bold text-3xl mt-3 tracking-[1px] lg:text-xl'>
                   {prod.name}
                 </h1>
                 <SeeProductButton
