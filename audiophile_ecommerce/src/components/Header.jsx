2bc8adcc5cbec45c2d292112d1b268aa3c890fbc
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-02 14:15:59 +0100

    tablet responsive for Header component

--- audiophile_ecommerce/src/components/Header.jsx
+++ audiophile_ecommerce/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = ({ text }) => {
   return (
-    <div className='w-full h-48 bg-[#141414] grid place-items-center'>
-      <h1 className='text-light-100 text-3xl font-semibold tracking-[1px]'>
+    <div className='w-full bg-[#141414] grid place-items-center relative top-28 h-64 lg:top-0 lg:h-48'>
+      <h1 className='text-light-100 text-4xl font-semibold tracking-[3px] lg:text-3xl'>
         {text}
       </h1>
     </div>
