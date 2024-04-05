394d9d4746b6cc33fc39e2d381d3865beadb14ca
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-05 21:43:38 +0100

    implemented mobile responsiveness for single product component

--- audiophile_ecommerce/src/components/ProductDetails.jsx
+++ audiophile_ecommerce/src/components/ProductDetails.jsx
@@ -14,34 +14,34 @@ const ProductDetails = ({ data }) => {
 
   return (
     <div
-      className={`flex w-full h-[40rem] gap-[4rem] lg:h-[30rem] lg:gap-[7rem]`}
+      className={`flex flex-col w-full h-fit gap-[2.5rem] sm:gap-[4rem] sm:h-[40rem] sm:flex-row lg:h-[30rem] lg:gap-[7rem]`}
     >
-      <aside className='w-1/2 h-full rounded-md bg-light-300 grid place-items-center lg:w-1/2'>
+      <aside className='w-full h-[25rem] rounded-xl bg-light-300 grid place-items-center sm:h-full sm:w-1/2'>
         <img
           src={img}
           alt={`${name} Image`}
           className='w-3/4 drop-shadow-2xl lg:w-1/2'
         />
       </aside>
-      <section className='w-1/2 h-full flex flex-col justify-center text-start items-start'>
+      <section className='w-full h-full flex flex-col justify-center text-start items-start sm:w-1/2'>
         {isNewProduct && (
           <p className='text-orange-200 text-base tracking-[10px] mb-5 lg:text-xs'>
             NEW PRODUCT
           </p>
         )}
-        <h1 className='text-4xl font-bold tracking-[2px] leading-10 lg:leading-9 mb-10 lg:mb-6 lg:text-3xl'>
+        <h1 className='text-4xl font-bold tracking-[2px] leading-10 mb-10 lg:leading-9 lg:mb-6 lg:text-3xl'>
           {name} <br />
           {category.toUpperCase()}
         </h1>
-        <p className='text-xl text-[#00000077] font-semibold leading-8 tracking-[1px] lg:pr-8 lg:leading-6 lg:text-sm'>
+        <p className='text-[19px] text-[#00000077] font-semibold leading-8 tracking-[1px] lg:pr-8 lg:leading-6 lg:text-sm'>
           {desc}
         </p>
 
-        <p className='font-bold mb-10 mt-7 text-2xl lg:text-base'>
+        <p className='font-bold mb-12 mt-9 text-[1.4rem] sm:mb-10 sm:mt-7 sm:text-2xl lg:text-base'>
           $ {curr(price)}
         </p>
         <section className='flex gap-3'>
-          <div className='flex gap-4 bg-light-300 px-5 py-4 font-bold justify-between w-36 text-[1.5rem] lg:text-base lg:w-24 lg:px-3 lg:py-2'>
+          <div className='flex gap-4 bg-light-300 px-5 py-4 font-bold justify-between w-36 text-xl sm:text-[1.5rem] lg:text-base lg:w-24 lg:px-3 lg:py-2'>
             <button
               className='text-[#00000062] btn'
               onClick={() => decreaseCount(data)}
@@ -62,7 +62,7 @@ const ProductDetails = ({ data }) => {
               totalAmount();
               totalCartItem();
             }}
-            className='btn px-6 w-full bg-orange-200 hover:bg-orange-100 text-light-100 font-bold text-[1.15rem] lg:w-fit lg:text-xs'
+            className='btn px-6 w-full bg-orange-200 hover:bg-orange-100 text-light-100 font-bold text-[1rem] sm:text-[1.15rem] lg:w-fit lg:text-xs'
           >
             ADD TO CART
           </button>
--- audiophile_ecommerce/src/pages/SingleproductPage.jsx
+++ audiophile_ecommerce/src/pages/SingleproductPage.jsx
@@ -16,36 +16,38 @@ export default function SingleproductPage() {
   // console.log(data);
 
   return (
-    <div className='container pt-[10rem] lg:pt-[6rem]'>
+    <div className='container pt-[7rem] sm:pt-[10rem] lg:pt-[6rem]'>
       <button
         onClick={goBack}
-        className='text-2xl text-[#00000062] mb-8 font-bold tracking-[1px] lg:mb-16 lg:text-[15px]'
+        className='text-lg text-[#00000062] mb-8 font-bold tracking-[1px] sm:text-2xl lg:mb-16 lg:text-[15px]'
       >
         Go Back
       </button>
 
-      <div className='mb-[11rem]'>
+      <div className='mb-24 sm:mb-[11rem]'>
         <ProductDetails data={data} />
       </div>
 
       <section className='flex flex-col justify-between lg:flex-row'>
         <div className='flex flex-col gap-10 w-full lg:w-[55%]'>
-          <h1 className='text-[2.5rem] font-bold lg:text-3xl'>FEATURES</h1>
-          <article className='flex flex-col gap-8 text-[1.35rem] lg:text-[14.5px] text-[#00000077] font-semibold'>
+          <h1 className='text-[2rem] font-bold sm:text-[2.5rem] lg:text-3xl'>
+            FEATURES
+          </h1>
+          <article className='flex flex-col gap-8 text-[1.2rem] sm:text-[1.35rem] lg:text-[14.5px] text-[#00000077] font-semibold'>
             <p>{data.features.point1}</p>
             <p>{data.features.point2}</p>
           </article>
         </div>
-        <div className='flex gap-10 mt-28 lg:flex-col lg:mt-0 lg:w-[45%] lg:pl-[8rem]'>
-          <h1 className='text-[2.5rem] font-bold text-start lg:text-3xl'>
+        <div className='flex gap-10 mt-28 flex-col sm:flex-row lg:flex-col lg:mt-0 lg:w-[45%] lg:pl-[8rem]'>
+          <h1 className='text-[2rem] font-bold text-start sm:text-[2.5rem] lg:text-3xl'>
             IN THE BOX
           </h1>
-          <aside className='flex flex-col gap-2'>
+          <aside className='flex flex-col gap-1 sm:gap-2'>
             {data.box.map((prod, i) => {
               return (
                 <div
                   key={i}
-                  className='flex gap-6 text-[1.35rem] ml-24 mt-4 lg:mt-0 lg:text-[14.5px] lg:ml-0'
+                  className='flex gap-6 text-[1.2rem] mt-1 sm:ml-24 sm:mt-4 sm:text-[1.35rem] lg:mt-0 lg:text-[14.5px] lg:ml-0'
                 >
                   <h1 className='text-orange-200 font-bold'>{prod.size}</h1>
                   <p className='text-[#00000077] font-semibold'>{prod.item}</p>
@@ -56,32 +58,40 @@ export default function SingleproductPage() {
         </div>
       </section>
 
-      <article className='w-full h-[30rem] flex gap-4 mt-[8.5rem] lg:gap-8 lg:h-[37rem]'>
-        <div className='w-[40%] h-full flex flex-col justify-between gap-4 lg:gap-8 lg:w-[45%]'>
-          <img src={data.images.image1} alt='images' className='h-full' />
-          <img src={data.images.image2} alt='images' className='h-full' />
+      <article className='w-full h-fit flex flex-col gap-4 mt-[8.5rem] sm:h-[30rem] sm:flex-row lg:gap-8 lg:h-[37rem]'>
+        <div className='w-full h-fit flex flex-col justify-between gap-4 sm:h-full sm:w-[40%] lg:gap-8 lg:w-[45%]'>
+          <img
+            src={data.images.image1}
+            alt='images'
+            className='h-[17rem] sm:h-full'
+          />
+          <img
+            src={data.images.image2}
+            alt='images'
+            className='h-[17rem] sm:h-full'
+          />
         </div>
-        <div className='w-[60%] h-full rounded-md lg:w-[55%]'>
+        <div className='w-full h-full rounded-md sm:w-[60%] lg:w-[55%]'>
           <img
             src={data.images.image3}
             alt='images'
-            className='w-full h-full object-cover rounded-lg'
+            className='w-full object-cover rounded-lg h-[25rem] sm:h-full'
           />
         </div>
       </article>
 
       <section className='mt-[11rem] mb-[4.5rem]'>
-        <h1 className='mb-12 text-center font-bold text-[2.5rem] tracking-[1px] lg:text-3xl'>
+        <h1 className='mb-12 text-center font-bold text-[2rem] tracking-[1px] sm:text-[2.5rem] lg:text-3xl'>
           YOU MAY ALSO LIKE
         </h1>
-        <div className='grid grid-cols-3 gap-4 lg:gap-9'>
+        <div className='grid gap-20 sm:grid-cols-3 sm:gap-4 lg:gap-9'>
           {data.mayAlsoLike.map((prod, i) => {
             return (
               <div
                 key={i}
-                className='flex flex-col justify-center items-center gap-8'
+                className='flex flex-col justify-center items-center gap-12 sm:gap-8'
               >
-                <aside className='flex justify-center items-center h-[22rem] rounded-md bg-light-300 w-full lg:h-[18rem]'>
+                <aside className='flex justify-center items-center h-fit rounded-xl bg-light-300 w-full py-8 sm:py-0 sm:h-[22rem] lg:h-[18rem]'>
                   <img src={prod.img} alt='' className='w-[9rem] lg:w-[8rem]' />
                 </aside>
                 <h1 className='font-bold text-3xl mt-3 tracking-[1px] lg:text-xl'>
@@ -96,8 +106,9 @@ export default function SingleproductPage() {
           })}
         </div>
       </section>
-
-      <ShopProducts />
+      <div className='pt-40 sm:pt-36 sm:pb-5'>
+        <ShopProducts />
+      </div>
     </div>
   );
 }
