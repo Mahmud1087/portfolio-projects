a35bb4468f0f7a270c39cdeed0ad50dc971e0232
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-05 16:06:19 +0100

    implemented mobile responsiveness for the hero section

--- audiophile_ecommerce/src/components/SeeProductButton.jsx
+++ audiophile_ecommerce/src/components/SeeProductButton.jsx
@@ -4,7 +4,7 @@ const SeeProductButton = ({ classes, to }) => {
   return (
     <Link
       to={`/${to}`}
-      className={`text-[1rem] tracking-[2px] w-fit transition-all delay-100 ${classes} font-semibold px-10 py-5 lg:px-7 lg:py-3 lg:text-xs`}
+      className={`text-xs tracking-[2px] w-fit transition-all delay-100 ${classes} font-semibold px-7 py-3 sm:text-[1rem] sm:px-10 sm:py-5 lg:px-7 lg:py-3 lg:text-xs`}
     >
       SEE PRODUCT
     </Link>
--- audiophile_ecommerce/src/components/home page/Hero.jsx
+++ audiophile_ecommerce/src/components/home page/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <div
       className={`flex bg-[#141414] hero_height overflow-hidden relative z-10`}
     >
-      <div className='absolute w-full h-full -top-12 lg:hidden'>
+      <div className='absolute w-full h-full -top-6 sm:-top-12 lg:hidden'>
         <img
           src={herobg}
           alt='background image of a headphone'
@@ -15,16 +15,17 @@ export default function Hero() {
       </div>
       <aside className='relative container flex w-full'>
         <section className='hero_height w-full flex flex-col justify-center items-center text-center text-light-100 lg:items-start lg:text-start lg:w-2/5'>
-          <h3 className='text-[#ffffff62] text-2xl tracking-[10px] mb-7 lg:mb-5 lg:text-xs'>
+          <h3 className='text-[#ffffff62] text-sm tracking-[10px] mb-5 sm:mb-7 sm:text-2xl lg:mb-5 lg:text-xs'>
             NEW PRODUCT
           </h3>
-          <h1 className='text-7xl tracking-[3px] font-bold leading-[5rem] mb-10 lg:mb-6 lg:leading-[3.3rem] lg:text-[2.9rem]'>
+          <h1 className='text-[2.6rem] tracking-[3px] font-bold leading-[3rem] mb-6 sm:mb-10 sm:leading-[5rem] sm:text-7xl lg:mb-6 lg:leading-[3.3rem] lg:text-[2.9rem]'>
             XX99 MARK II <br />
             HEADPHONES
           </h1>
-          <p className='text-[1.4rem] text-[#ffffffb9] tracking-[1px] mb-16 leading-9 lg:mb-7 lg:leading-[25px] lg:text-[12.5px]'>
-            Experience natural, lifelike audio and exceptional <br /> build
-            quality made for the passionate music <br /> enthusiast.
+          <p className='text-[16px] text-[#ffffffb9] tracking-[1px] mb-10 leading-6 sm:mb-16 sm:leading-9 sm:text-[1.4rem] lg:mb-7 lg:leading-[25px] lg:text-[12.5px]'>
+            Experience natural, lifelike audio and exceptional{' '}
+            <br className='hidden sm:block' /> build quality made for the
+            passionate music <br className='hidden sm:block' /> enthusiast.
           </p>
           <SeeProductButton
             to='XX99 MK II'
