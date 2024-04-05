af17366aa5c98fac1066f79877cee973fe96e4bf
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-05 12:46:48 +0100

    Animate sidebar (halfway)

--- audiophile_ecommerce/src/components/Sidebar.jsx
+++ audiophile_ecommerce/src/components/Sidebar.jsx
@@ -1,11 +1,32 @@
 import { ShopProducts } from '../components/main';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useProductsContex } from '../context/products_context';
 
 export default function Sidebar() {
+  const { isSideBarOpen } = useProductsContex();
+
   return (
-    <div className='fixed w-screen h-screen bg-[#00000060] z-50'>
-      <section className='relative top-[7rem] h-fit w-full bg-light-100 rounded-xl pb-24 px-12'>
-        <ShopProducts />
-      </section>
-    </div>
+    <AnimatePresence mode='wait'>
+      {isSideBarOpen && (
+        <motion.div
+          key={'parent'}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className='fixed w-screen h-screen bg-[#00000060] z-50'
+        >
+          <motion.section
+            key='sidebarAnimate'
+            initial={{ y: '-100%', opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ type: 'spring', stiffness: 50 }}
+            exit={{ y: '-100%', opacity: 0 }}
+            className='relative top-[7rem] h-fit w-full bg-light-100 rounded-b-xl pb-24 px-8'
+          >
+            <ShopProducts />
+          </motion.section>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
