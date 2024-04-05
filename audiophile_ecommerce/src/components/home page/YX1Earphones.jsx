3fb230b895f817035ff84d1712efccffb3daa808
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-05 17:39:45 +0100

    implemented mobile responsiveness for the YX1Earphones component

--- audiophile_ecommerce/src/components/home page/YX1Earphones.jsx
+++ audiophile_ecommerce/src/components/home page/YX1Earphones.jsx
@@ -4,22 +4,22 @@ import { SeeProductButton } from '../../components/main';
 
 const YX1Earphones = () => {
   return (
-    <div className='flex container gap-x-5 lg:gap-x-6 pt-12'>
-      <section className='w-1/2 rounded-lg h-[18rem]'>
+    <div className='flex flex-col container gap-x-5 gap-y-5 sm:gap-y-0 pt-8 sm:flex-row lg:gap-x-6 sm:pt-12'>
+      <section className='w-full rounded-lg h-[18rem] sm:w-1/2'>
         <img
           src={airpodImg1}
           alt='Earbud image'
-          className='hidden w-full h-full object-contain rounded-[inherit] lg:block'
+          className='block w-full h-full object-cover rounded-[inherit] sm:hidden lg:block lg:object-contain'
         />
         <img
           src={airpodImg2}
           alt='Earbud image'
-          className='w-full h-full object-center rounded-[inherit] lg:hidden'
+          className='w-full h-full object-center rounded-[inherit] hidden sm:block lg:hidden'
         />
       </section>
-      <section className='w-1/2 rounded-lg h-[18rem] bg-light-300 flex items-center pl-8 lg:pl-24'>
-        <div className='flex flex-col justify-center gap-12 lg:gap-6'>
-          <h1 className='text-[2rem] font-bold tracking-[1px] text-center lg:text-start lg:text-2xl'>
+      <section className='w-full rounded-lg h-[18rem] bg-light-300 flex items-center pl-8 sm:w-1/2 lg:pl-24'>
+        <div className='flex flex-col justify-center gap-8 sm:gap-12 lg:gap-6'>
+          <h1 className='text-3xl font-bold tracking-[1px] text-center sm:text-[2rem] lg:text-start lg:text-2xl'>
             YX1 EARPHONES
           </h1>
           <SeeProductButton
