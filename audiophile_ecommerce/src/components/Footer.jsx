3de58a58c084cfa70aa920d496e1fd5deafe83d6
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-06 16:02:23 +0100

    bug fix

--- audiophile_ecommerce/src/components/Footer.jsx
+++ audiophile_ecommerce/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
         <section className='container'>
           <div className='w-[6rem] m-auto h-[5px] bg-orange-200 rounded-full sm:m-0 lg:h-[3px]'></div>
           <article className='flex flex-col mt-20 justify-center items-center sm:justify-start sm:items-start'>
-            <div className='flex flex-col justify-between gap-14 lg:gap-0 lg:flex-row'>
+            <div className='flex flex-col justify-between gap-14 lg:w-full lg:gap-0 lg:flex-row'>
               <Logo />
               <NavLinks
                 Fsize='text-[17px] lg:text-[12px]'
@@ -20,7 +20,7 @@ export default function Footer() {
               />
             </div>
 
-            <div className='flex justify-between mt-10 text-[#ffffff6e]'>
+            <div className='flex justify-between mt-10 text-[#ffffff6e] lg:w-full'>
               <p className='text-xl leading-9 text-center mt-12 sm:mt-0 sm:text-start sm:text-[1.35rem] lg:leading-6 lg:text-sm'>
                 Audiophile is an all in one stop to fulfill your audio needs.
                 We're a small team <br className='hidden lg:block' />
@@ -35,7 +35,7 @@ export default function Footer() {
               </div>
             </div>
 
-            <div className='flex flex-col items-center justify-between mt-28 gap-y-16 sm:mt-32 sm:gap-y-0 sm:flex-row lg:mt-20'>
+            <div className='flex flex-col items-center justify-between mt-28 gap-y-16 sm:w-full sm:mt-32 sm:gap-y-0 sm:flex-row lg:mt-20'>
               <p className='text-[#ffffff60] text-xl text-center sm:text-start sm:text-2xl lg:text-sm'>
                 &copy; Copyright Enesi 2024. All Rights Reserved
               </p>
