c5a9ca49864ca4b15262fc22bbbccd8984b27a34
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-06 10:48:49 +0100

    implemented mobile responsiveness for the cart box component

--- audiophile_ecommerce/src/components/cart/Cart.jsx
+++ audiophile_ecommerce/src/components/cart/Cart.jsx
@@ -18,19 +18,19 @@ const Cart = () => {
   return (
     <div className='fixed h-screen w-screen top-0 left-0 bg-transparent z-50'>
       <section className='fixed w-full hero_height top-20 left-0 bg-[#00000060]'>
-        <article className='relative z-40 top-12 container lg:top-6'>
+        <article className='relative z-40 top-8 container sm:top-12 lg:top-6'>
           <div
             className={`absolute right-0 ${
-              cartList.length === 0 ? 'h-auto' : 'h-[42rem]'
-            } w-[33rem] bg-light-100 rounded-md py-7 px-9 flex flex-col lg:h-[30rem] lg:w-[24rem]`}
+              cartList.length === 0 ? 'h-auto' : 'h-[31rem] sm:h-[42rem]'
+            } w-full bg-light-100 rounded-lg py-7 px-6 flex flex-col sm:px-9 sm:w-[33rem] lg:h-[30rem] lg:w-[24rem]`}
           >
             <header className='flex justify-between items-center font-bold mb-9 lg:mb-6'>
-              <h1 className='text-2xl tracking-[1px] lg:text-sm'>
+              <h1 className='text-lg tracking-[1px] sm:text-2xl lg:text-sm'>
                 CART <span>({cartList.length})</span>
               </h1>
               <button
                 onClick={clearCartItems}
-                className='text-[#00000077] btn text-xl lg:text-[13px]'
+                className='text-[#00000077] btn text-lg underline sm:text-xl lg:text-[13px]'
               >
                 Remove all
               </button>
@@ -40,7 +40,7 @@ const Cart = () => {
               className={`custom-scroll h-full overflow-y-scroll overflow-x-hidden mb-7 flex flex-col gap-5`}
             >
               {cartList.length === 0 && (
-                <h1 className='text-dark-300 font-bold text-3xl my-12 lg:text-base'>
+                <h1 className='text-dark-300 font-bold text-xl my-12 sm:text-3xl lg:text-base'>
                   No Item Added
                 </h1>
               )}
@@ -51,7 +51,7 @@ const Cart = () => {
                     className='flex justify-between items-center'
                   >
                     <CartItems {...prod} />
-                    <div className='flex gap-7 items-center bg-light-300 font-bold justify-between text-2xl px-5 py-2 lg:gap-5 lg:px-3 lg:py-1 lg:text-base'>
+                    <div className='flex gap-3 items-center bg-light-300 font-bold justify-between text-lg px-2 py-1 sm:gap-7 sm:text-2xl sm:px-5 sm:py-2 lg:gap-5 lg:px-3 lg:py-1 lg:text-base'>
                       <button
                         className='text-[#00000062] btn'
                         onClick={() => {
@@ -61,7 +61,9 @@ const Cart = () => {
                       >
                         -
                       </button>
-                      <h1 className='text-xl lg:text-xs'>{prod.amount}</h1>
+                      <h1 className='text-sm sm:text-xl lg:text-xs'>
+                        {prod.amount}
+                      </h1>
                       <button
                         className='text-[#00000062] btn'
                         onClick={() => {
@@ -80,11 +82,11 @@ const Cart = () => {
             <section
               className={`${cartList.length === 0 ? 'hidden' : 'block'}`}
             >
-              <div className='flex justify-between items-center mb-10 lg:mb-5'>
-                <h1 className='text-2xl text-[#00000077] font-bold lg:text-[13px]'>
+              <div className='flex justify-between items-center mb-6 sm:mb-10 lg:mb-5'>
+                <h1 className='text-xl text-[#00000077] font-bold sm:text-2xl lg:text-[13px]'>
                   TOTAL
                 </h1>
-                <h1 className='font-bold text-2xl lg:text-sm'>
+                <h1 className='font-bold text-xl sm:text-2xl lg:text-sm'>
                   $ {curr(cartTotal)}
                 </h1>
               </div>
@@ -94,7 +96,7 @@ const Cart = () => {
                   vatAndShippingFee();
                   toggleCart();
                 }}
-                className='btn block text-center w-full p-5 bg-orange-200 hover:bg-orange-100 text-light-100 font-bold tracking-[1px] text-2xl transition-all delay-75 lg:p-3 lg:text-xs'
+                className='btn block text-center w-full p-3 bg-orange-200 hover:bg-orange-100 text-light-100 font-bold tracking-[1px] text-lg transition-all delay-75 sm:text-2xl sm:p-5 lg:p-3 lg:text-xs'
               >
                 CHECKOUT
               </Link>
--- audiophile_ecommerce/src/components/cart/CartItems.jsx
+++ audiophile_ecommerce/src/components/cart/CartItems.jsx
@@ -2,13 +2,19 @@ import { curr } from '../../helper';
 
 const CartItems = ({ img, shortName, price }) => {
   return (
-    <section className='flex gap-5 items-center lg:gap-3'>
-      <aside className='w-24 h-24 p-4 bg-light-300 rounded-md flex justify-center items-center lg:w-16 lg:h-16'>
-        <img src={img} alt='product image' className='w-4/5 lg:w-full' />
+    <section className='flex gap-3 items-center sm:gap-5 lg:gap-3'>
+      <aside className='w-16 h-16 p-2 bg-light-300 rounded-xl flex justify-center items-center sm:h-24 sm:w-24 lg:w-16 lg:h-16'>
+        <img
+          src={img}
+          alt='product image'
+          className='w-3/5 sm:w-4/5 lg:w-full'
+        />
       </aside>
       <div className='flex flex-col gap-1'>
-        <h1 className='text-2xl font-bold lg:text-xs'>{shortName}</h1>
-        <p className='text-lg font-bold text-[#00000077] lg:text-[10.5px]'>
+        <h1 className='text-lg font-bold sm:text-2xl lg:text-xs'>
+          {shortName}
+        </h1>
+        <p className='text-sm font-bold text-[#00000077] sm:text-lg lg:text-[10.5px]'>
           $ {curr(price)}
         </p>
       </div>
