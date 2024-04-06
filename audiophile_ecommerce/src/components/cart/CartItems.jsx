73ce0c7b8967e61d0f72136a46487961657c363c
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-06 16:24:48 +0100

    edit

--- audiophile_ecommerce/src/components/cart/CartItems.jsx
+++ audiophile_ecommerce/src/components/cart/CartItems.jsx
@@ -4,11 +4,7 @@ const CartItems = ({ img, shortName, price }) => {
   return (
     <section className='flex gap-3 items-center sm:gap-5 lg:gap-3'>
       <aside className='w-16 h-16 p-2 bg-light-300 rounded-xl flex justify-center items-center sm:h-24 sm:w-24 lg:w-16 lg:h-16'>
-        <img
-          src={img}
-          alt='product image'
-          className='w-3/5 sm:w-4/5 lg:w-full'
-        />
+        <img src={img} alt='product image' className='w-3/5 sm:w-4/5' />
       </aside>
       <div className='flex flex-col gap-1'>
         <h1 className='text-lg font-bold sm:text-2xl lg:text-xs'>
