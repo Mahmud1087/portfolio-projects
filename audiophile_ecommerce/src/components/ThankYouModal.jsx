dc7541c17da630c651e0d72e207237c521e7e658
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-07 11:07:53 +0100

    added animation to the cart modal and the thank you modal using framer motion, the exit fuction is not working well yet

--- audiophile_ecommerce/src/components/ThankYouModal.jsx
+++ audiophile_ecommerce/src/components/ThankYouModal.jsx
@@ -2,68 +2,102 @@ import { FaCheck } from 'react-icons/fa';
 import CartItems from './cart/CartItems';
 import { useProductsContex } from '../context/products_context';
 import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 import { curr } from '../helper';
 
 const ThankYouModal = () => {
   const { cartList, hideModal, cartTotal, vatFee, shippingFee } =
     useProductsContex();
 
+  const dropIn = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    visible: {
+      scale: [0.2, 0.4, 0.6, 1],
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: 'spring',
+        stiffness: 50,
+      },
+    },
+    exit: {
+      scale: 0,
+      opacity: 0,
+    },
+  };
+
   return (
-    <div className='fixed h-screen w-screen top-0 left-0 bg-transparent'>
-      <section className='fixed overflow-scroll w-full hero_height left-0 bg-[#00000060] sm:top-20'>
-        <article className='relative z-50 h-full w-full flex justify-center container top-28 sm:top-32 lg:top-0 lg:overflow-hidden lg:grid lg:place-items-center'>
-          <div className='absolute bg-light-100 rounded-md py-8 px-6 flex flex-col justify-between w-full h-fit sm:py-9 sm:px-12 sm:h-[42rem] sm:w-[42rem] lg:py-7 lg:px-9 lg:h-[29rem] lg:w-[27rem]'>
-            {cartList.length === 0 ? (
-              <h1 className='w-full h-full grid place-items-center text-xl font-bold sm:text-2xl'>
-                Oops! you have no order
-              </h1>
-            ) : (
-              <div className='flex flex-col'>
-                <aside className='w-fit rounded-full bg-orange-200 p-4 mb-7 sm:mb-10 lg:mb-6'>
-                  <FaCheck className='text-light-100 text-2xl sm:text-4xl lg:text-lg' />
-                </aside>
-                <h1 className='font-bold tracking-[1px] text-3xl leading-9 mb-4 sm:leading-[3.3rem] sm:text-5xl sm:mb-6 lg:mb-4 lg:leading-8 lg:text-[1.7rem]'>
-                  THANK YOU <br />
-                  FOR YOUR ORDER
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className='fixed h-screen w-screen top-0 left-0 bg-transparent'
+      >
+        <section className='fixed overflow-scroll w-full hero_height left-0 bg-[#00000060] sm:top-20'>
+          <motion.article
+            variants={dropIn}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            className='relative z-50 h-full w-full flex justify-center container top-28 sm:top-32 lg:top-0 lg:overflow-hidden lg:grid lg:place-items-center'
+          >
+            <div className='absolute bg-light-100 rounded-md py-8 px-6 flex flex-col justify-between w-full h-fit sm:py-9 sm:px-12 sm:h-[42rem] sm:w-[42rem] lg:py-7 lg:px-9 lg:h-[29rem] lg:w-[27rem]'>
+              {cartList.length === 0 ? (
+                <h1 className='w-full h-full grid place-items-center text-xl font-bold sm:text-2xl'>
+                  Oops! you have no order
                 </h1>
-                <p className='text-[#00000077] font-semibold mb-9 sm:text-xl lg:text-[13px] lg:mb-4'>
-                  You will receive an email confirmation shortly.
-                </p>
-                <section className='flex w-full flex-col sm:h-48 sm:flex-row sm:py-0 lg:h-fit'>
-                  <div className='w-full rounded-t-lg bg-light-300 h-full flex flex-col justify-center py-6 sm:rounded-l-md sm:rounded-tr-none sm:py-0 sm:w-3/5 lg:justify-start lg:h-fit lg:py-3'>
-                    <div className='flex justify-between pr-10 pl-5 pb-3 sm:pr-7 sm:pl-3'>
-                      <CartItems {...cartList[0]} />
-                      <p className='text-[#00000077] font-bold py-1 sm:text-lg sm:py-6 lg:text-xs lg:py-3'>
-                        x{cartList[0].amount}
+              ) : (
+                <div className='flex flex-col'>
+                  <aside className='w-fit rounded-full bg-orange-200 p-4 mb-7 sm:mb-10 lg:mb-6'>
+                    <FaCheck className='text-light-100 text-2xl sm:text-4xl lg:text-lg' />
+                  </aside>
+                  <h1 className='font-bold tracking-[1px] text-3xl leading-9 mb-4 sm:leading-[3.3rem] sm:text-5xl sm:mb-6 lg:mb-4 lg:leading-8 lg:text-[1.7rem]'>
+                    THANK YOU <br />
+                    FOR YOUR ORDER
+                  </h1>
+                  <p className='text-[#00000077] font-semibold mb-9 sm:text-xl lg:text-[13px] lg:mb-4'>
+                    You will receive an email confirmation shortly.
+                  </p>
+                  <section className='flex w-full flex-col sm:h-48 sm:flex-row sm:py-0 lg:h-fit'>
+                    <div className='w-full rounded-t-lg bg-light-300 h-full flex flex-col justify-center py-6 sm:rounded-l-md sm:rounded-tr-none sm:py-0 sm:w-3/5 lg:justify-start lg:h-fit lg:py-3'>
+                      <div className='flex justify-between pr-10 pl-5 pb-3 sm:pr-7 sm:pl-3'>
+                        <CartItems {...cartList[0]} />
+                        <p className='text-[#00000077] font-bold py-1 sm:text-lg sm:py-6 lg:text-xs lg:py-3'>
+                          x{cartList[0].amount}
+                        </p>
+                      </div>
+                      <hr className='text-[#00000025] mb-3 w-[80%] mx-auto sm:py-0' />
+                      <p className='text-center text-[#00000077] font-bold sm:text-xl lg:text-xs'>
+                        and {cartList.length - 1} other item(s)
                       </p>
                     </div>
-                    <hr className='text-[#00000025] mb-3 w-[80%] mx-auto sm:py-0' />
-                    <p className='text-center text-[#00000077] font-bold sm:text-xl lg:text-xs'>
-                      and {cartList.length - 1} other item(s)
-                    </p>
-                  </div>
-                  <div className='w-full rounded-b-lg bg-dark-300 h-[7rem] flex flex-col justify-center px-8 sm:h-full sm:py-0 sm:rounded-r-md sm:rounded-bl-none sm:w-2/5 lg:px-4'>
-                    <p className='text-[#ffffff86] text-lg sm:text-xl lg:text-xs'>
-                      GRAND TOTAL
-                    </p>
-                    <p className='text-light-100 text-lg font-bold mt-3 sm:text-xl lg:text-xs'>
-                      $ {curr(cartTotal + vatFee + shippingFee)}
-                    </p>
-                  </div>
-                </section>
-              </div>
-            )}
-            <Link
-              to='/'
-              onClick={hideModal}
-              className='btn bg-orange-200 py-4 text-center text-light-100 text-lg font-bold tracking-[1px] hover:bg-orange-100 mt-7 sm:text-xl sm:py-5 sm:mt-0 lg:py-3 lg:text-[13px]'
-            >
-              BACK TO HOME
-            </Link>
-          </div>
-        </article>
-      </section>
-    </div>
+                    <div className='w-full rounded-b-lg bg-dark-300 h-[7rem] flex flex-col justify-center px-8 sm:h-full sm:py-0 sm:rounded-r-md sm:rounded-bl-none sm:w-2/5 lg:px-4'>
+                      <p className='text-[#ffffff86] text-lg sm:text-xl lg:text-xs'>
+                        GRAND TOTAL
+                      </p>
+                      <p className='text-light-100 text-lg font-bold mt-3 sm:text-xl lg:text-xs'>
+                        $ {curr(cartTotal + vatFee + shippingFee)}
+                      </p>
+                    </div>
+                  </section>
+                </div>
+              )}
+              <Link
+                to='/'
+                onClick={hideModal}
+                className='btn bg-orange-200 py-4 text-center text-light-100 text-lg font-bold tracking-[1px] hover:bg-orange-100 mt-7 sm:text-xl sm:py-5 sm:mt-0 lg:py-3 lg:text-[13px]'
+              >
+                BACK TO HOME
+              </Link>
+            </div>
+          </motion.article>
+        </section>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 export default ThankYouModal;
--- audiophile_ecommerce/src/components/cart/Cart.jsx
+++ audiophile_ecommerce/src/components/cart/Cart.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useProductsContex } from '../../context/products_context';
 import { curr } from '../../helper';
 import CartItems from './CartItems';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Cart = () => {
   const {
@@ -14,99 +15,136 @@ const Cart = () => {
     increaseCount,
     totalAmount,
     vatAndShippingFee,
+    isCartOpen,
   } = useProductsContex();
+
+  const dropIn = {
+    hidden: {
+      y: '-100vh',
+      opacity: 0,
+    },
+    visible: {
+      y: '0',
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: 'spring',
+        stiffness: 50,
+      },
+    },
+    exit: {
+      y: '100vh',
+      opacity: 0,
+    },
+  };
+
   return (
-    <div className='fixed h-screen w-screen top-0 left-0 bg-transparent z-50'>
-      <section className='fixed w-full hero_height top-20 left-0 bg-[#00000060]'>
-        <article className='relative z-40 top-8 container sm:top-12 lg:top-6'>
-          <div
-            className={`absolute right-0 ${
-              cartList.length === 0
-                ? 'h-fit'
-                : 'max-h-[31rem] sm:max-h-[42rem] lg:max-h-[30rem]'
-            } w-full bg-light-100 rounded-lg py-7 px-6 flex flex-col sm:px-9 sm:w-[33rem] lg:w-[24rem]`}
-          >
-            <header className='flex justify-between items-center font-bold mb-9 lg:mb-6'>
-              <h1 className='text-lg tracking-[1px] sm:text-2xl lg:text-sm'>
-                CART <span>({cartList.length})</span>
-              </h1>
-              <button
-                onClick={clearCartItems}
-                className='text-[#00000077] btn text-lg underline sm:text-xl lg:text-[13px]'
-              >
-                Remove all
-              </button>
-            </header>
-
-            <aside
-              className={`custom-scroll h-full overflow-y-scroll overflow-x-hidden mb-7 flex flex-col gap-5`}
+    <AnimatePresence mode='wait' initial={true}>
+      {isCartOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className='fixed h-screen w-screen top-0 left-0 bg-transparent z-50'
+        >
+          <section className='fixed w-full hero_height top-20 left-0 bg-[#00000060]'>
+            <motion.article
+              variants={dropIn}
+              initial='hidden'
+              animate='visible'
+              exit='exit'
+              className='relative z-40 top-8 container sm:top-12 lg:top-6'
             >
-              {cartList.length === 0 && (
-                <h1 className='text-dark-300 font-bold text-xl text-center sm:text-3xl lg:text-base'>
-                  No Item Added
-                </h1>
-              )}
-              {cartList.map((prod) => {
-                return (
-                  <div
-                    key={prod.id}
-                    className='flex justify-between items-center'
+              <div
+                className={`absolute right-0 ${
+                  cartList.length === 0
+                    ? 'h-fit'
+                    : 'max-h-[31rem] sm:max-h-[42rem] lg:max-h-[30rem]'
+                } w-full bg-light-100 rounded-lg py-7 px-6 flex flex-col sm:px-9 sm:w-[33rem] lg:w-[24rem]`}
+              >
+                <header className='flex justify-between items-center font-bold mb-9 lg:mb-6'>
+                  <h1 className='text-lg tracking-[1px] sm:text-2xl lg:text-sm'>
+                    CART <span>({cartList.length})</span>
+                  </h1>
+                  <button
+                    onClick={clearCartItems}
+                    className='text-[#00000077] btn text-lg underline sm:text-xl lg:text-[13px]'
                   >
-                    <CartItems {...prod} />
-                    <div className='flex gap-3 items-center bg-light-300 font-bold justify-between text-lg px-2 py-1 sm:gap-7 sm:text-2xl sm:px-5 sm:py-2 lg:gap-5 lg:px-3 lg:py-1 lg:text-base'>
-                      <button
-                        className='text-[#00000062] btn'
-                        onClick={() => {
-                          decreaseCount(prod);
-                          totalAmount();
-                        }}
-                      >
-                        -
-                      </button>
-                      <h1 className='text-sm sm:text-xl lg:text-xs'>
-                        {prod.amount}
-                      </h1>
-                      <button
-                        className='text-[#00000062] btn'
-                        onClick={() => {
-                          increaseCount(prod);
-                          totalAmount();
-                        }}
-                      >
-                        +
-                      </button>
-                    </div>
-                  </div>
-                );
-              })}
-            </aside>
+                    Remove all
+                  </button>
+                </header>
 
-            <section
-              className={`${cartList.length === 0 ? 'hidden' : 'block'}`}
-            >
-              <div className='flex justify-between items-center mb-6 sm:mb-10 lg:mb-5'>
-                <h1 className='text-xl text-[#00000077] font-bold sm:text-2xl lg:text-[13px]'>
-                  TOTAL
-                </h1>
-                <h1 className='font-bold text-xl sm:text-2xl lg:text-sm'>
-                  $ {curr(cartTotal)}
-                </h1>
+                <aside
+                  className={`custom-scroll h-full overflow-y-scroll overflow-x-hidden mb-7 flex flex-col gap-5`}
+                >
+                  {cartList.length === 0 && (
+                    <h1 className='text-dark-300 font-bold text-xl text-center sm:text-3xl lg:text-base'>
+                      No Item Added
+                    </h1>
+                  )}
+                  {cartList.map((prod) => {
+                    return (
+                      <div
+                        key={prod.id}
+                        className='flex justify-between items-center'
+                      >
+                        <CartItems {...prod} />
+                        <div className='flex gap-3 items-center bg-light-300 font-bold justify-between text-lg px-2 py-1 sm:gap-7 sm:text-2xl sm:px-5 sm:py-2 lg:gap-5 lg:px-3 lg:py-1 lg:text-base'>
+                          <button
+                            className='text-[#00000062] btn'
+                            onClick={() => {
+                              decreaseCount(prod);
+                              totalAmount();
+                            }}
+                          >
+                            -
+                          </button>
+                          <h1 className='text-sm sm:text-xl lg:text-xs'>
+                            {prod.amount}
+                          </h1>
+                          <button
+                            className='text-[#00000062] btn'
+                            onClick={() => {
+                              increaseCount(prod);
+                              totalAmount();
+                            }}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </aside>
+
+                <section
+                  className={`${cartList.length === 0 ? 'hidden' : 'block'}`}
+                >
+                  <div className='flex justify-between items-center mb-6 sm:mb-10 lg:mb-5'>
+                    <h1 className='text-xl text-[#00000077] font-bold sm:text-2xl lg:text-[13px]'>
+                      TOTAL
+                    </h1>
+                    <h1 className='font-bold text-xl sm:text-2xl lg:text-sm'>
+                      $ {curr(cartTotal)}
+                    </h1>
+                  </div>
+                  <Link
+                    to='/checkout'
+                    onClick={() => {
+                      vatAndShippingFee();
+                      toggleCart();
+                    }}
+                    className='btn block text-center w-full p-3 bg-orange-200 hover:bg-orange-100 text-light-100 font-bold tracking-[1px] text-lg transition-all delay-75 sm:text-2xl sm:p-5 lg:p-3 lg:text-xs'
+                  >
+                    CHECKOUT
+                  </Link>
+                </section>
               </div>
-              <Link
-                to='/checkout'
-                onClick={() => {
-                  vatAndShippingFee();
-                  toggleCart();
-                }}
-                className='btn block text-center w-full p-3 bg-orange-200 hover:bg-orange-100 text-light-100 font-bold tracking-[1px] text-lg transition-all delay-75 sm:text-2xl sm:p-5 lg:p-3 lg:text-xs'
-              >
-                CHECKOUT
-              </Link>
-            </section>
-          </div>
-        </article>
-      </section>
-    </div>
+            </motion.article>
+          </section>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 export default Cart;
