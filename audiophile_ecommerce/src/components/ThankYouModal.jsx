4077a6d7e7dba87ac3c794f66a797c99f07b0318
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-09 15:21:25 +0100

    reduced the font sizes of the texts and some images on the thank you page for mobile screens

--- audiophile_ecommerce/src/components/ThankYouModal.jsx
+++ audiophile_ecommerce/src/components/ThankYouModal.jsx
@@ -48,41 +48,41 @@ const ThankYouModal = () => {
               exit='exit'
               className='relative z-50 h-full w-full flex justify-center container top-28 sm:top-32 lg:top-0 lg:overflow-hidden lg:grid lg:place-items-center'
             >
-              <div className='absolute bg-light-100 rounded-md py-8 px-6 flex flex-col justify-between w-full h-fit sm:py-9 sm:px-12 sm:h-[42rem] sm:w-[42rem] lg:py-7 lg:px-9 lg:h-[29rem] lg:w-[27rem]'>
+              <div className='absolute bg-light-100 rounded-md py-8 px-8 flex flex-col justify-between w-full h-fit sm:py-9 sm:px-12 sm:h-[42rem] sm:w-[42rem] lg:py-7 lg:px-9 lg:h-[29rem] lg:w-[27rem]'>
                 {cartList.length === 0 ? (
-                  <h1 className='w-full h-full grid place-items-center text-xl font-bold sm:text-2xl'>
+                  <h1 className='w-full h-full grid place-items-center text-sm font-bold sm:text-2xl'>
                     Oops! you have no order
                   </h1>
                 ) : (
                   <div className='flex flex-col'>
-                    <aside className='w-fit rounded-full bg-orange-200 p-4 mb-7 sm:mb-10 lg:mb-6'>
-                      <FaCheck className='text-light-100 text-2xl sm:text-4xl lg:text-lg' />
+                    <aside className='w-fit rounded-full bg-orange-200 p-3 mb-5 sm:mb-10 lg:mb-6'>
+                      <FaCheck className='text-light-100 text-lg sm:text-4xl lg:text-lg' />
                     </aside>
-                    <h1 className='font-bold tracking-[1px] text-3xl leading-9 mb-4 sm:leading-[3.3rem] sm:text-5xl sm:mb-6 lg:mb-4 lg:leading-8 lg:text-[1.7rem]'>
+                    <h1 className='font-bold tracking-[1px] text-xl leading-6 mb-4 sm:leading-[3.3rem] sm:text-5xl sm:mb-6 lg:mb-4 lg:leading-8 lg:text-[1.7rem]'>
                       THANK YOU <br />
                       FOR YOUR ORDER
                     </h1>
-                    <p className='text-[#00000077] font-semibold mb-9 sm:text-xl lg:text-[13px] lg:mb-4'>
+                    <p className='text-[#00000077] font-semibold mb-7 text-[13px] sm:text-xl sm:mb-9 lg:text-[13px] lg:mb-4'>
                       You will receive an email confirmation shortly.
                     </p>
                     <section className='flex w-full flex-col sm:h-48 sm:flex-row sm:py-0 lg:h-fit'>
                       <div className='w-full rounded-t-lg bg-light-300 h-full flex flex-col justify-center py-6 sm:rounded-l-md sm:rounded-tr-none sm:py-0 sm:w-3/5 lg:justify-start lg:h-fit lg:py-3'>
                         <div className='flex justify-between pr-10 pl-5 pb-3 sm:pr-7 sm:pl-3'>
                           <CartItems {...cartList[0]} />
-                          <p className='text-[#00000077] font-bold py-1 sm:text-lg sm:py-6 lg:text-xs lg:py-3'>
+                          <p className='text-[#00000077] font-bold py-2 text-xs sm:text-lg sm:py-6 lg:text-xs lg:py-3'>
                             x{cartList[0].amount}
                           </p>
                         </div>
                         <hr className='text-[#00000025] mb-3 w-[80%] mx-auto sm:py-0' />
-                        <p className='text-center text-[#00000077] font-bold sm:text-xl lg:text-xs'>
+                        <p className='text-center text-[#00000077] font-bold text-xs sm:text-xl lg:text-xs'>
                           and {cartList.length - 1} other item(s)
                         </p>
                       </div>
-                      <div className='w-full rounded-b-lg bg-dark-300 h-[7rem] flex flex-col justify-center px-8 sm:h-full sm:py-0 sm:rounded-r-md sm:rounded-bl-none sm:w-2/5 lg:px-4'>
-                        <p className='text-[#ffffff86] text-lg sm:text-xl lg:text-xs'>
+                      <div className='w-full rounded-b-lg bg-dark-300 h-[6rem] flex flex-col justify-center px-8 sm:h-full sm:py-0 sm:rounded-r-md sm:rounded-bl-none sm:w-2/5 lg:px-4'>
+                        <p className='text-[#ffffff86] text-[13px] sm:text-xl lg:text-xs'>
                           GRAND TOTAL
                         </p>
-                        <p className='text-light-100 text-lg font-bold mt-3 sm:text-xl lg:text-xs'>
+                        <p className='text-light-100 text-[13px] font-bold mt-3 sm:text-xl lg:text-xs'>
                           $ {curr(cartTotal + vatFee + shippingFee)}
                         </p>
                       </div>
@@ -92,7 +92,7 @@ const ThankYouModal = () => {
                 <Link
                   to='/'
                   onClick={hideModal}
-                  className='btn bg-orange-200 py-4 text-center text-light-100 text-lg font-bold tracking-[1px] hover:bg-orange-100 mt-7 sm:text-xl sm:py-5 sm:mt-0 lg:py-3 lg:text-[13px]'
+                  className='btn bg-orange-200 py-4 text-center text-light-100 text-xs font-bold tracking-[1px] hover:bg-orange-100 mt-7 sm:text-xl sm:py-5 sm:mt-0 lg:py-3 lg:text-[13px]'
                 >
                   BACK TO HOME
                 </Link>
