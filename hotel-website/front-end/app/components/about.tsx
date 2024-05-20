df6eb9f455b20f284b30934d390853602bebd397
Mahmud1087 <abdulazeezm578@gmail.com>
2024-05-20 11:03:29 +0100

    edit: edited the about page

--- hotel-website/front-end/app/components/about.tsx
+++ hotel-website/front-end/app/components/about.tsx
@@ -1,11 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
+import Image from 'next/image';
 import Button from './button';
 import SectionTitle from './sectionTitle';
+import { nanoid } from 'nanoid';
+
+const imageData = [
+  {
+    src: '/aboutImg2.png',
+    alt: 'About Image 1',
+  },
+  {
+    src: '/aboutImg3.png',
+    alt: 'About Image 2',
+  },
+  //   {
+  //     src: '/aboutImg1.png',
+  //     alt: 'About Image 2',
+  //   },
+];
 
 const About = () => {
   return (
     <div className='bg-gray-200 py-20'>
-      <div className='w-[90%] mx-auto flex'>
+      <div className='w-[90%] mx-auto flex items-center'>
         <section className='w-1/2'>
           <SectionTitle color='text-gray-900'>about us</SectionTitle>
           <p className='w-[80%] mb-10'>
@@ -22,7 +39,34 @@ const About = () => {
           </div>
         </section>
 
-        <section className='w-1/2 bg-black'></section>
+        <section className='relative w-1/2 h-[30rem] flex flex-col gap-3'>
+          <article className=' grid grid-cols-2 gap-3 h-1/2 w-full'>
+            {imageData.map((item) => {
+              return (
+                <div key={nanoid()} className='w-full h-full group'>
+                  <Image
+                    src={item.src}
+                    alt={item.alt}
+                    width={1000}
+                    height={1000}
+                    quality={100}
+                    className='w-full h-full object-cover rounded-xl'
+                  />
+                </div>
+              );
+            })}
+          </article>
+          <div className=' w-full h-1/2 flex justify-center'>
+            <Image
+              src='/aboutImg1.png'
+              alt='about image'
+              width={1000}
+              height={1000}
+              quality={100}
+              className='w-1/2 h-full object-cover rounded-xl'
+            />
+          </div>
+        </section>
       </div>
     </div>
   );
